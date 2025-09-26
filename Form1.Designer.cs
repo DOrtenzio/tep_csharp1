e044e33a042814898c48a469c08f925c4e05ff78
Diego D'Ortenzio <diego.dortenzio.studente@itispaleocapa.it>
2025-09-26 18:02:24 +0200

    Aggiungere i file di progetto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace app1
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Label lblCounter;
+        private System.Windows.Forms.Button btnIncrement;
+        private System.Windows.Forms.Button btnDecrement;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
+
+        /// <summary>
+        /// Pulizia delle risorse in uso.
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        private void InitializeComponent()
+        {
+            this.lblCounter = new System.Windows.Forms.Label();
+            this.btnIncrement = new System.Windows.Forms.Button();
+            this.btnDecrement = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblCounter
+            // 
+            this.lblCounter.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblCounter.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Bold);
+            this.lblCounter.Location = new System.Drawing.Point(0, 0);
+            this.lblCounter.Name = "lblCounter";
+            this.lblCounter.Size = new System.Drawing.Size(400, 80);
+            this.lblCounter.TabIndex = 0;
+            this.lblCounter.Text = "0";
+            this.lblCounter.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnIncrement
+            // 
+            this.btnIncrement.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnIncrement.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.btnIncrement.Location = new System.Drawing.Point(3, 2);
+            this.btnIncrement.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnIncrement.Name = "btnIncrement";
+            this.btnIncrement.Size = new System.Drawing.Size(127, 44);
+            this.btnIncrement.TabIndex = 0;
+            this.btnIncrement.Text = "➕ Incrementa";
+            this.btnIncrement.UseVisualStyleBackColor = true;
+            this.btnIncrement.Click += new System.EventHandler(this.btn_increment);
+            // 
+            // btnDecrement
+            // 
+            this.btnDecrement.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnDecrement.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.btnDecrement.Location = new System.Drawing.Point(136, 2);
+            this.btnDecrement.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnDecrement.Name = "btnDecrement";
+            this.btnDecrement.Size = new System.Drawing.Size(127, 44);
+            this.btnDecrement.TabIndex = 1;
+            this.btnDecrement.Text = "➖ Decrementa";
+            this.btnDecrement.UseVisualStyleBackColor = true;
+            this.btnDecrement.Click += new System.EventHandler(this.btn_decrement);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnReset.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.btnReset.Location = new System.Drawing.Point(269, 2);
+            this.btnReset.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(128, 44);
+            this.btnReset.TabIndex = 2;
+            this.btnReset.Text = "🔄 Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btn_reset);
+            // 
+            // tableLayoutPanel
+            // 
+            this.tableLayoutPanel.ColumnCount = 3;
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.34F));
+            this.tableLayoutPanel.Controls.Add(this.btnIncrement, 0, 0);
+            this.tableLayoutPanel.Controls.Add(this.btnDecrement, 1, 0);
+            this.tableLayoutPanel.Controls.Add(this.btnReset, 2, 0);
+            this.tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tableLayoutPanel.Location = new System.Drawing.Point(0, 112);
+            this.tableLayoutPanel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tableLayoutPanel.Name = "tableLayoutPanel";
+            this.tableLayoutPanel.RowCount = 1;
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel.Size = new System.Drawing.Size(400, 48);
+            this.tableLayoutPanel.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 160);
+            this.Controls.Add(this.lblCounter);
+            this.Controls.Add(this.tableLayoutPanel);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Contatore";
+            this.tableLayoutPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace app1 { //package
+    public partial class Form1 : Form { //Classe parziale su più file, che estende Form ovvero una gui
+        private int contatore;
+        public Form1() {  //costruttore
+            InitializeComponent(); //scene inizialize
+            this.contatore = 0;
+        }
+
+        protected void increment() { 
+            this.contatore++;
+            writeOnLabel();
+        }
+        protected void decrement() { 
+            this.contatore--;
+            writeOnLabel();
+        }
+        protected void reset() { 
+            this.contatore=0;
+            writeOnLabel();
+        }
+
+        private void writeOnLabel(){ lblCounter.Text = contatore.ToString(); }
+
+        //Handler o metodi collegati ad eventi
+        private void btn_increment(object sender, EventArgs e) {
+            this.increment();
+        }
+
+        private void btn_decrement(object sender, EventArgs e) {
+            this.decrement();
+        }
+
+        private void btn_reset(object sender, EventArgs e) {
+            this.reset();
+        }
+    }
+}
